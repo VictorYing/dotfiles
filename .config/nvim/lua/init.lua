6072de069bdb9785e455eef3a4fd95bf7984efa9
Victor A. Ying <victory@csail.mit.edu>
2022-08-30 17:00:00 -0400

    [config/nvim] Highlight yanked region
    A neat little built-in Neovim feature

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -1,3 +1,14 @@
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
