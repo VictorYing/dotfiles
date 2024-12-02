9e141e2e55e15b6a6eb4992b9e92bed982a65b33
Victor Ying <vying@tenstorrent.com>
2024-12-01 20:27:18 -0600

    Apparently this merge script doesn't work with mawk?

--- .bin/merge_history.sh
+++ .bin/merge_history.sh
@@ -4,7 +4,7 @@
 combined_file=$(mktemp)
 for histfile in ${DOTFILES_HOME}/.bash/histories/* ~/.bash_history; do
     [ -f "$histfile" ] || continue;
-    awk 'NR%2{printf "%s ",$0;next;}1' "$histfile" >> "$combined_file"
+    gawk 'NR%2{printf "%s ",$0;next;}1' "$histfile" >> "$combined_file"
 done
 
 # Sort by timestamp and uniq
@@ -12,6 +12,6 @@ sorted_file=$(mktemp)
 sort -u "$combined_file" > "$sorted_file"
 
 # Split the combined lines back into the original format
-awk '{print $1; sub($1" ",""); print}' "$sorted_file" > ~/.bash_history
+gawk '{print $1; sub($1" ",""); print}' "$sorted_file" > ~/.bash_history
 
 rm "$combined_file" "$sorted_file"
